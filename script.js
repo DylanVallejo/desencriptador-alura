ce84201fcaee841087c4935765502b8dbe9285f4
DylanVallejo <dylan1811vallejo@gmail.com>
2023-02-20 01:36:10 -0500

    añadiendoe un evento input y click para lamacenar el valor y mostarlo al dar click en desencriptar

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 
 
-console.log("hola alura")
+// const displayMessage = function(message) {
+//     document.querySelector('.encriptar').textContent = message;
+// }
 
-function hola(){
-    
+
+let inputArea  =  document.querySelector('.textAreaContainer');
+let texto = document.getElementsByClassName('.valorIngresado');
+
+inputArea.addEventListener('input', updateValue);
+function updateValue(event) {
+    console.log(texto)
+    texto =  event.target.value;
 }
+
+document.querySelector('.btnEncriptar').addEventListener
+('click', function (){
+    // console.log(texto)
+    console.log(texto)
+    document.querySelector('.valorIngresado').innerHTML = texto
+    // const texto = document.querySelector('.btnEncriptar').textContent
+    // console.log("hola" + texto)    
+})
