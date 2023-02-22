6ede75be9c21514fc81e6fcc8fe415fc3b410dd4
DylanVallejo <dylan1811vallejo@gmail.com>
2023-02-21 21:30:34 -0500

    agregada funcion encriptadora de texto y validaciones de acentos y mayusculas mostrando el mensaje al hacer click en desencriptar

--- script.js
+++ script.js
@@ -1,72 +1,100 @@
 
 
 
-// Título de su sitio web
-// Campo para el texto que va a ser encriptado/desencriptado
-// Un botón para encriptar
-// Un botón para desencriptar
-// Área para mostrar el texto encriptado/desencriptado.
-// Un rodapie con los datos de la persona que desarrolló el sitio web!
-
-
 let inputArea  =  document.querySelector('.textAreaContainer');
 let texto = document.getElementsByClassName('.valorIngresado');
 
+
+// creando un evento listener para catualizar el valor cada que se modifica
 inputArea.addEventListener('input', updateValue);
 function updateValue(event) {
-    console.log(texto)
+    //texto alamcenara el valor del input 
     texto =  event.target.value;
 }
 
-document.querySelector('.btnEncriptar').addEventListener
-('click', function (){
+
+//la funcion cifrar reemplazara los elementos iterando con un for loop 
+function cifrar(texto){
+
+    //creo mis variables para los objetos a encriptar y desencriptar
+    let codedValues =  {
+        a: "ai",
+        e: "enter",
+        i: "imes",
+        o: "ober",
+        u: "ufat"
+    }
+    let valoreDesencriptados = {
+        
+        ai: "a",
+        enter: "e",
+        imes: "i",
+        ober: "o",
+        ufat: "u"
+        
+    }
     
+    //creo una variable nueva para manipular el array de strings creado por el metodo split
+    let newString  = texto.split("");
+    //itero el array y condiciono para las vocales que requiero cambiar 
+
+    let error = ""
+
+    // - Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+    
+    for(let i= 0; i < newString.length; i++){
+        
+    // /[^\u0000-\u007F]/.test(newString[i])  define the regex in this case fot upper case and use the method test 
+    // this return a boolean if includes accents in the text
+        if(newString[i] === newString[i].toUpperCase() && newString[i] !== " " || /[^\u0000-\u007F]/.test(newString[i])  ){
+            error = "El texto ingresado no debe contener mayusculas o caracteres especiales" ;
+        }else {
+            if(newString[i].includes("a")){
+                newString[i] = codedValues.a
+            }else if(newString[i].includes("e")){
+                newString[i] = codedValues.e
+            }else if(newString[i].includes("e")){
+                newString[i] = codedValues.i
+            }else if(newString[i].includes("o")){
+                newString[i] = codedValues.i
+            }
+            else if(newString[i].includes("u")){
+                newString[i] = codedValues.u
+            }
+        }
+    }
+    return error !== "" ?  error : newString.join("");
+    
+}
+
+document.querySelector('.btnEncriptar').addEventListener
+('click', async function (){
     
     if(texto.length === 0 ){
         document.querySelector('.valorIngresado').innerHTML = "No has ingresado un texto para encriptar"
     }else{
+        const cifrado = await cifrar(texto);
+        console.log(cifrado)
         
-        document.querySelector('.valorIngresado').innerHTML = texto
+        document.querySelector('.valorIngresado').innerHTML = cifrado   
+    }
+})
+
+document.querySelector('.btnDesencriptar').addEventListener
+('click', async function (){
+    
+    if(texto.length === 0 ){
+        document.querySelector('.valorIngresado').innerHTML = "No has ingresado un texto para encriptar"
+    }else{
+        const cifrado = await cifrar(texto);        
+        document.querySelector('.valorIngresado').innerHTML = cifrado   
     }
 })
 
 
 
 
-// Description
-// ¡Bienvenidos y Bienvenidas a nuestro primer desafío!
 
-// Durante estas cuatro semanas, vamos a trabajar en una aplicación que encripta textos, así podrás intercambiar mensajes secretos con otras personas que sepan el secreto de la encriptación utilizada.
 
-// Las "llaves" de encriptación que utilizaremos son las siguientes:
-
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
-
-// Requisitos:
-// - Debe funcionar solo con letras minúsculas
-// - No deben ser utilizados letras con acentos ni caracteres especiales
-// - Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
-
-// Por ejemplo:
-// "gato" => "gaitober"
-// gaitober" => "gato"
-
-// La página debe tener campos para
-// inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre as dos opciones.
-// El resultado debe ser mostrado en la pantalla.
 // Extras:
 // - Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
-
-// Tenemos un periodo de tiempo de cuatro semanas para desarrollar el proyecto y vamos a trabajar con el sistema ágil de desarrollo, utilizando el Trello de la siguiente forma:
-
-// La columna Listos para iniciar presenta las tarjetas con elementos que aun no fueron desarrollados.
-// En la columna En Desarrollo estarán los elementos que estés desarrollando en el momento. Al iniciar una tarea, podrás mover la tarjeta que contiene dicha tarea para esta columna.
-// En la columna Pausado estarán los elementos que comenzaste a desarrollar, pero necesitaste parar por algún motivo.
-// Por fin, en la columna Concluido estarán los elementos ya concluidos.
-// El Trello es una herramienta de uso individual para que puedas controlar el progreso de tus actividades, pero no será evaluada.
-
-// Buen proyecto!
